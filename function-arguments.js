1f104cc44d09edf19c0b4f48cde12e9b1b46c509
Nikita Singla <nikitasingla712@gmail.com>
2019-11-21 02:48:59 +0530

    javascripting

---
+++ function-arguments.js
@@ -0,0 +1,9 @@
+var a=53 , b=61 , c=67
+var mul,result
+function math(a,b,c)
+{
+  mul=b*c
+  result=a+mul
+  return result
+}
+console.log(math(a,b,c))
---
+++ functions.js
@@ -0,0 +1,6 @@
+var food ='bananas'
+function eat(food)
+{
+  return food + ' tasted really good.'
+}
+console.log(eat(food))
